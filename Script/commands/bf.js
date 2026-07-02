bc73838df1fd7b5f26d2811a488fe920b308908e
SAHU UHAS <sahubbz19@gmail.com>
2026-07-02 22:34:02 +0600

    Update bf.js

--- Script/commands/bf.js
+++ Script/commands/bf.js
@@ -34,7 +34,7 @@ module.exports.run = async function ({ event, api }) {
 
   try {
     const apiList = await axios.get(
-      "https://raw.githubusercontent.com/shahadat-sahu/SAHU-API/main/SAHU-API.json"
+      "https://raw.githubusercontent.com/sahu-uhas/SAHU-API/refs/heads/main/API.json"
     );
 
     const AVATAR_CANVAS_API = apiList.data.AvatarCanvas;
